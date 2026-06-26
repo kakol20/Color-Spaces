c3e9d2f9bca287a6c767a13325396b1c59e852c4
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2026-06-26 03:33:51 +0100

    add scalingMethodBothways function

--- src/dev.js
+++ src/dev.js
@@ -1,5 +1,5 @@
 const devTools = {
-	scalingMethod: function (hex1 = '#0000ff', hex2 = '#ffff00', count = 256, method = OkLCh.HueMethod.Longer) {
+	scalingMethod: function (hex1 = '#0000ff', hex2 = '#ffff00', count = 512, method = OkLCh.HueMethod.Longer) {
 		// const expectedC = (col2.c - col1.c) * t + col1.c;
 		// let newC = (offset.actualC - col2.c * offset.t) / (1 - offset.t);
 		// let newC = (offset.actualC - col1.c) / offset.t + col1.c;
@@ -18,6 +18,66 @@ const devTools = {
 			if (expectedC <= 0) continue;
 
 			const col_OkLCh = OkLCh.HueInterpolate(col1_OkLCh, col2_OkLCh, t, method);
+			const actualC = col_OkLCh.c;
+
+			const currentScale = actualC / expectedC;
+
+			if (currentScale < minScale) minScale = currentScale;
+		}
+
+		let newCol1_OkLCh = col1_OkLCh.copy();
+		let newCol2_OkLCh = col2_OkLCh.copy();
+
+		newCol1_OkLCh.c *= minScale;
+		newCol2_OkLCh.c *= minScale;
+
+		const newCol1_sRGB = OkLCh.OkLChTosRGB(newCol1_OkLCh);
+		const newCol2_sRGB = OkLCh.OkLChTosRGB(newCol2_OkLCh);
+
+		console.log('Old', col1_OkLCh.CSSColor, col2_OkLCh.CSSColor);
+		console.log('New', newCol1_OkLCh.CSSColor, newCol2_OkLCh.CSSColor);
+		console.log(newCol1_sRGB.CSSColor, newCol2_sRGB.CSSColor);
+		console.log(sRGB.sRGBToHex(newCol1_sRGB), sRGB.sRGBToHex(newCol2_sRGB));
+	},
+
+	scalingMethodBothWays: function(hex1 = '#0000ff', hex2 = '#ffff00', count = 512, method = OkLCh.HueMethod.Longer) {
+		let method1 = method;
+		let method2 = method;
+		if (method === OkLCh.HueMethod.Longer || method === OkLCh.HueMethod.Longer) {
+			method1 = OkLCh.HueMethod.Longer;
+			method2 = OkLCh.HueMethod.Shorter;
+		} else {
+			method1 = OkLCh.HueMethod.Increasing;
+			method2 = OkLCh.HueMethod.Decreasing;
+		}
+
+		const col1_sRGB = sRGB.HexTosRGB(hex1);
+		const col2_sRGB = sRGB.HexTosRGB(hex2);
+
+		const col1_OkLCh = OkLCh.sRGBToOkLCh(col1_sRGB);
+		const col2_OkLCh = OkLCh.sRGBToOkLCh(col2_sRGB);
+
+		let minScale = 1;
+
+		for (let i = 0; i < count; ++i) {
+			const t = i / (count - 1);
+			const expectedC = (col2_OkLCh.c - col1_OkLCh.c) * t + col1_OkLCh.c;
+			if (expectedC <= 0) continue;
+
+			const col_OkLCh = OkLCh.HueInterpolate(col1_OkLCh, col2_OkLCh, t, method1);
+			const actualC = col_OkLCh.c;
+
+			const currentScale = actualC / expectedC;
+
+			if (currentScale < minScale) minScale = currentScale;
+		}
+
+		for (let i = 0; i < count; ++i) {
+			const t = i / (count - 1);
+			const expectedC = (col2_OkLCh.c - col1_OkLCh.c) * t + col1_OkLCh.c;
+			if (expectedC <= 0) continue;
+
+			const col_OkLCh = OkLCh.HueInterpolate(col1_OkLCh, col2_OkLCh, t, method2);
 			const actualC = col_OkLCh.c;
 
 			const currentScale = actualC / expectedC;
